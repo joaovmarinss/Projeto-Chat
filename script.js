22a25b64788a8acad16db0b9be9fd370d4b6a52f
Joao Vitor Marins <marinsjoao@id.uff.br>
2022-07-28 20:44:16 -0300

    feat:addconditionforsendandeditjs

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 const butsend = document.querySelector("#send")
+const text = document.querySelector("#textarea")
 butsend.addEventListener("click",() =>{
-    const mensagensbox = document.querySelector("#mensagens")
-    const text = document.querySelector("#textarea")
-    const ncontmen = document.createElement("div")
-    ncontmen.classList.add("contmen")
-    ncontmen.innerHTML = `<div class="textmen"><p>${text.value}<\p>
-        </div>
-        <div class="bottons">
-        <button class="btedit" onclick="edit(this)">Editar</button>
-        <button class="btexit" onclick="exit(this)">Excluir</button>
-    </div>`
-    mensagensbox.appendChild(ncontmen)
-    text.value = ""
+    if(!(text.value == "")){
+        const mensagensbox = document.querySelector("#mensagens")
+        const ncontmen = document.createElement("div")
+        ncontmen.classList.add("contmen")
+        ncontmen.innerHTML = `<div class="textmen"><p>${text.value}<\p>
+            </div>
+            <div class="bottons">
+            <button class="btedit" onclick="edit(this)">Editar</button>
+            <button class="btexit" onclick="exit(this)">Excluir</button>
+        </div>`
+        mensagensbox.appendChild(ncontmen)
+        text.value = ""
+    }else{
+        alert("Digite um texto")
+    }   
 })
 function edit(check){
-    const text = document.querySelector("#textarea")
-    check.parentNode.parentNode.firstChild.innerHTML = text.value
+    if(!(text.value == "")) {
+       check.parentNode.parentNode.firstChild.innerHTML = text.value
+    }else{
+        alert("Digite um texto")
+    }
 }   
 function exit(check){
     check.parentNode.parentNode.remove()
